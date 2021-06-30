e6c88b00cd03c079c159d9fe8246526c14da0933
Jambang J <70057710+jiopolm@users.noreply.github.com>
2021-06-30 15:13:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d years old", age)
+      
+      return 0;
+}

1adc96bcc35b9efe6fba25c3e08ecf0b95f00944
Jambang J <70057710+jiopolm@users.noreply.github.com>
2021-06-30 15:42:25 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-//user input response code
+//user input response code and the part i learnt after datatypes
 int main()
 {
     int age;
     printf("Enter your age: ");
     scanf("%d", &age);
-    printf("You are %d years old", age)
+    printf("You are %d years old", age);
       
       return 0;
 }
+
